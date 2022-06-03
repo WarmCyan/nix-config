1cea9272e66e584d0ace2a2915f6ae449e65b0b0
Martindale, Nathan <martindalena@ornl.gov>
2022-06-03 17:07:54 -0400

    Add basic neovim and zsh config

--- flake.nix
+++ flake.nix
@@ -9,6 +9,8 @@
 # ./result/bin/home-manager --flake ".#PROFILENAME" switch
 #
 # see also: https://nix-community.github.io/home-manager/index.html#sec-flakes-standalone
+#
+# it is necessary with kitty to export TERMINFO_DIRS=/usr/share/terminfo
 
 
 {
@@ -26,11 +28,166 @@
 
 		homeConfigurations.arcane = home-manager.lib.homeManagerConfiguration {
 			configuration = {pkgs, ...}: {
-				programs.home-manager.enable = true;
+
 				home.packages = [
 					pkgs.cowsay
-					pkgs.neovim
+					#pkgs.neovim
+					#pkgs.tmux
+					#pkgs.git
 				];
+				
+				targets.genericLinux.enable = true;
+
+
+				programs.home-manager.enable = true;
+				programs.zsh = {
+					enable = true;
+					shellAliases = {
+
+						# quick dir changes
+						".." = "cd ..";
+						"..." = "cd ...";
+						"...." = "cd ....";
+
+						# coloration
+						ls = "ls --color=auto";
+						grep = "grep --color=auto";
+						ll = "ls -lAh --color=auto";
+
+						# verbosity and human readable
+						df = "df -h";
+						du = "du -ch";
+						cp = "cp -v";
+						mv = "mv -v";
+
+						# don't destroy my computer
+						rm = "rm -Iv --preserve-root";
+						chown = "chown --preserve-root";
+						chmod = "chmod --preserve-root";
+						chgrp = "chgrp --preserve-root";
+
+						v = "nvim";
+						c = "clear";
+						q = "exit";
+						mx = "chmod +x";
+						lab = "cd ~/lab";
+
+						# tmux
+						tn = "tmux new-session -s";
+						ta = "tmux attach -t";
+						tl = "tmux ls";
+						tk = "tmux kill-session -t";
+						tc = "tmux new-session -t";
+
+						# git
+						g = "git status";
+						ga = "git add -A";
+						gc = "git commit -a";
+						gu = "git push";
+						gd = "git pull";
+						gl = "git log --oneline -n 10";
+
+					};
+				};
+				programs.git = {
+					enable = true;
+					userName = "Martindale, Nathan";
+					userEmail = "martindalena@ornl.gov";
+				};
+				programs.tmux = {
+					enable = true;
+					shell = "${pkgs.zsh}/bin/zsh";
+					aggressiveResize = true;
+					shortcut = "a";
+				};
+				programs.neovim = {
+					enable = true;
+
+					extraConfig = ''
+
+						if has('mouse')
+							set mouse=a
+							set mousehide " hide mouse when typing text
+						endif
+
+						" misc???
+						syntax on
+						filetype plugin indent on
+
+                        colorscheme monokai
+
+						" ==============================================================================
+						" SETTINGS
+						" ==============================================================================
+
+						" look
+						set number " line numbers!
+						set scrolloff=4 " keep 4 visible lines around cursorline when near top or bottom
+						set cursorline " bghighlight of current line
+						set title " window title
+						set titlestring=%t%(\ %M%)%(\ (%{expand(\"%:p:h\")})%)%(\ %a%)\ -\ %{v:servername}
+						set laststatus=2 " always show status line
+						set statusline=%t\ %m%*\ %y%h%r%w\ %<%F\ %*\ %=\ Lines:\ %L\ \ \ Col:\ %c\ \ \ [%n]
+						set noshowmode " mode unnecessary since shown in lightline
+
+						" search
+						set hlsearch " highlight search matches
+						set incsearch " move highlight as you add charachters to search string
+						set ignorecase " ignore case in search
+						set smartcase " ...unless being smart about it!
+
+						" tabs
+						set tabstop=4 " number of columns used for a tab
+						set shiftwidth=4 " how columns indent operations (<<, >>) use
+						set softtabstop=4 " how many spaces used when hit tab in insert mode
+						set expandtab " I've given up the fight on spaces v tabs... :/
+
+
+						" ==============================================================================
+						" KEY BINDINGS
+						" ==============================================================================
+
+						" ESC to leave insert mode is terrible! 'jk' is much nicer
+						inoremap jk <SPACE><BS><ESC>
+						inoremap JK <SPACE><BS><ESC>
+						inoremap Jk <SPACE><BS><ESC>
+
+						" make ',' find next character, like ';' normally does
+						nnoremap , ;
+
+						" press ';' in normal mode instead of ':', it's too common to use shift all the time!
+						nnoremap ; :
+						vnoremap ; :
+
+						" better window navigation
+						noremap <C-h> <C-w>h
+						noremap <C-j> <C-w>j
+						noremap <C-k> <C-w>k
+						noremap <C-l> <C-w>l
+					'';
+					
+					plugins = with pkgs.vimPlugins; [
+						vim-nix
+                        vim-monokai
+                        { 
+                            plugin = lightline-vim;
+                            config = ''let g:lightline = { 
+                                \ 'colorscheme': 'powerline', 
+                                \ 'active': {
+                                    \ 'left': [ [ 'mode', 'paste' ],
+                                              \ [ 'readonly', 'filename', 'modified'] ],
+                                    \ 'right': [ [ 'lineinfo' ],
+                                               \ [ 'percent', 'linecount' ],
+                                               \ [ 'fileformat', 'fileencoding', 'filetype' ] ]'
+                                \ },
+                                \ 'component': {
+                                    \ 'linecount': '%L'
+                                \ },
+                            \ }
+                            '';
+                        }
+					];
+				};
 			};
 			system = "x86_64-linux";
 			homeDirectory = "/home/81n";
