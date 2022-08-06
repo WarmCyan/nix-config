3773c797d0ea1122ea47b2974ccfa112593cdecc
Nathan Martindale <nathanamartindale@gmail.com>
2022-08-06 09:55:36 -0400

    More progress on templates

--- pkgs/testing2.nix
+++ pkgs/testing2.nix
@@ -3,7 +3,13 @@ builders.writeTemplatedShellApplication {
   name = "testing2";
   version = "0.2.0";
   usage = "testing2 [-t|--testing] [--yessir VALUE]";
-  text = "echo \"sup dawg \${thing1-} \${yessir-yo}\"";
+  text = ''
+    echo "sup dawg ''${thing1-} ''${yessir-yo}"
+
+    if [[ $# -gt 0 ]]; then
+      echo "$@"
+    fi
+  '';
   description = "things";
   parameters = {
     thing1 = { 
