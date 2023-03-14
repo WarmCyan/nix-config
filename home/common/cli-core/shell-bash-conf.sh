d58c4cbe3faebdbb6496dcbea64196f32f029ab4
Martindale, Nathan <martindalena@ornl.gov>
2023-03-14 11:30:18 -0400

    Fix missing escape characters in bash prompt

--- home/common/cli-core/shell-bash-conf.sh
+++ home/common/cli-core/shell-bash-conf.sh
@@ -3,9 +3,11 @@
 # ==================================================
 
 # prepare colors
-readonly ta_none="$(tput sgr0 2> /dev/null || true)"
-readonly ta_bold="$(tput bold 2> /dev/null || true)"
-readonly fg_blue="$(tput setaf 4 2> /dev/null || true)"
+# NOTE: the \001 and \002 are from https://unix.stackexchange.com/a/447520
+# without, the bash line doesn't wrap correctly
+readonly ta_none="\001$(tput sgr0 2> /dev/null || true)\002"
+readonly ta_bold="\001$(tput bold 2> /dev/null || true)\002"
+readonly fg_blue="\001$(tput setaf 4 2> /dev/null || true)\002"
     
 # nice prompt
 PS1="[${fg_blue}\t${ta_none}] ${fg_blue}\u${ta_normal}@${fg_blue}${ta_bold}\h${ta_none}${fg_blue} \w${ta_none} $ " 
