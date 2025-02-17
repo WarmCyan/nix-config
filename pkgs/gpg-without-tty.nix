3ad49d1c5f8541dddab639afaf66f4c5469e1893
Martindale, Nathan <nathanamartindale@gmail.com>
2025-02-16 22:37:42 -0500

    Attempted changes

--- pkgs/gpg-without-tty.nix
+++ pkgs/gpg-without-tty.nix
@@ -7,14 +7,16 @@ writeShellApplication {
     export pflag=""
     # create dynamic `--passphrase` flag to insert into the final command if the passphrase variable is not empty.
     if [[ -n "$GPG_PASS" ]];  then
-      pflag="--passphrase ${GPG_PASS}"
+      # shellcheck disable=SC2089
+      pflag="--passphrase ''\'''${GPG_PASS}'"
     fi
 
     # "<&0" → use same stdin as the one originally piped to script
     # "$@" → pass all script arguments to actual command
 
+    # shellcheck disable=SC2086,SC2068,SC2090
     gpg --yes --batch $pflag $@ <&0
 
     exit $?
   '';
-};
+}
