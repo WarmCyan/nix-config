8ca9e18a1cc7dde3cf8583a9a749336ec553fd99
Martindale, Nathan <martindalena@ornl.gov>
2025-02-19 16:45:15 -0500

    Add auto-title and prompt

--- pkgs/cg.nix
+++ pkgs/cg.nix
@@ -1,10 +1,20 @@
+# TODO: tmux integration w custom tmux
 # TODO: can't break out of env loop if env already activated
+# TODO: modify both query line and tmux window title (if relevant)
+# TODO: specify whether to check conda env or some configed folder of git repos
 { pkgs, builders }:
 builders.writeTemplatedShellApplication {
   name = "cg";
   version = "0.1.0";
   description = "code grep - tool to search micromamba env site package code.";
-  usage = "cg [-h|--help] [--version]";
+  usage = "cg [-t|--tmux] [-h|--help] [--version]";
+  # parameters = {
+  #   tmux = {
+  #     flags = [ "-t" "--tmux" ];
+  #     description = "Entering on target line of code opens the file in a new tmux window instead of replacing current.";
+  #     option = true;
+  #   };
+  # };
   runtimeInputs = [
     pkgs.ripgrep
     pkgs.fzf
@@ -37,13 +47,15 @@ builders.writeTemplatedShellApplication {
           # shellcheck disable=SC2164
           cd "''${env_loc}/lib"
           lib_path="$(rg --sort-files --null --files python3.*/site-packages/* --max-depth=1 -g '!**/*.dist-info/**' | xargs -0 dirname | uniq | fzf)"
-          
+
           # shellcheck disable=SC2181
           if [ $? -ne 0 ]; then
             continue_lib=0
           else
             continue_search=1
 
+            lib_name="$(echo ''\"''${lib_path}''\" | sed -e 's/.*\/\([A-Za-z0-9\-\_]*\)$/\1/g')"
+            echo -e "\033]0;''${lib_name}\007"
 
             # shellcheck disable=SC2181
             while [ $continue_search -eq 1 ]
@@ -51,7 +63,7 @@ builders.writeTemplatedShellApplication {
               echo "''${lib_path}"
               # shellcheck disable=SC2164
               cd "''${env_loc}/lib/''${lib_path}"
-              file_line="$(rg --column --color=always --line-number --no-heading --smart-case . | fzf --ansi --delimiter=: --preview='bat --color=always {1} --highlight-line {2}' --preview-window '+{2}-2,~3' )"
+              file_line="$(rg --column --color=always --line-number --no-heading --smart-case . | fzf --ansi --delimiter=: --preview='bat --color=always {1} --highlight-line {2}' --preview-window '+{2}-2,~3' --prompt=''\"''${lib_name}> ''\")"
 
               # shellcheck disable=SC2181
               if [ $? -ne 0 ]; then
