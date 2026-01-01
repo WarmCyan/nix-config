0b34b8713b0e4583b966d393976c0069465d7ce7
Martindale, Nathan <nathanamartindale@gmail.com>
2026-01-01 17:33:06 -0500

    Get manual wireguard assignments working

--- hosts/therock/cyan-network-config.nix
+++ hosts/therock/cyan-network-config.nix
@@ -13,43 +13,59 @@ in
 
   networking.firewall.allowedUDPPorts = [ 51830 ];
 
-  # networking.wireguard = {
-  #   enable = true;
-  #   interfaces = {
-  #     # wg1 = {
-  #     #   ips = [ "192.168.200.1/32" ];
-  #     #
-  #     #   listenPort = 51830;
-  #     #
-  #     #   privateKeyFile = "/etc/wg/private_key";
-  #     #   generatePrivateKeyFile = true;
-  #     # };
-  #   };
-  # };
-
-  services.wg-access-server = {
+  networking.wireguard = {
     enable = true;
-    settings = {
-      https.enabled = false;
-      dns.enabled = false;
-      vpn.allowedIPs = [ "192.168.200.0/24" ];
-      vpn.cidr = "192.168.200.0/24";
-      wireguard.port = 51830;
-      wireguard.interface = "wg2";
-      port = 8000;
-      loglevel = "debug";
-      vpn.cidrv6 = 0;
-      vpn.nat66 = false;
-      externalHost = "192.168.1.31"; # eventually set to external ip
-      httpHost = "192.168.200.1";
+    interfaces = {
+      wg2 = {
+        ips = [ "192.168.200.1/32" ];
+      
+        listenPort = 51830;
+      
+        privateKeyFile = "/etc/wg/private_key";
+        # generatePrivateKeyFile = true;
+        peers = [
+          {
+            allowedIPs = [ "192.168.200.4/32" "192.168.130.3/32" ];
+            publicKey = "6jHY+DAfq2xpKbsW4+H8FOX3z+MB9rZLvhiL24oJzgQ=";
+          }
+          {
+            allowedIPs = [ "192.168.200.5/32" "192.168.130.8/32" ];
+            publicKey = "Hwi4/lYNRUYbicDmbYXQZJ6md8YzGhedK7XkbxQwOQg=";
+          }
+          {
+            allowedIPs = [ "192.168.200.2/32" ];
+            # publicKey = "tlt353h0+X0wkCjnp2aDTK4dwvES3Iy60tjXfloQyVI=";
+            publicKey = "/DcVm728bh5+7/EIdU2i+x0hwAE/iHcN9S3p35BgNDY=";
+          }
+        ];
+      };
     };
-    secretsFile = "/etc/wg/wg-access-server-secrets";
   };
 
+  # services.wg-access-server = {
+  #   enable = true;
+  #   settings = {
+  #     https.enabled = false;
+  #     dns.enabled = false;
+  #     vpn.allowedIPs = [ "192.168.200.0/24" "192.168.130.3/32" "192.168.130.8/32" ];
+  #     vpn.cidr = "192.168.200.0/24";
+  #     wireguard.port = 51830;
+  #     wireguard.interface = "wg2";
+  #     port = 8000;
+  #     loglevel = "debug";
+  #     vpn.cidrv6 = 0;
+  #     vpn.nat66 = false;
+  #     externalHost = "192.168.1.31"; # eventually set to external ip
+  #     httpHost = "192.168.200.1";
+  #   };
+  #   secretsFile = "/etc/wg/wg-access-server-secrets";
+  # };
+  #
   programs.tcpdump.enable = true;
 
   environment.systemPackages = with pkgs; [
     unixtools.net-tools
+    wireguard-tools
   ];
   
   services.nginx = {
