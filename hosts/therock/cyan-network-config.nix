7f0a1cea9cfa01eedc2c71db6f9a63cece11afd7
Martindale, Nathan <nathanamartindale@gmail.com>
2026-01-03 00:11:43 -0500

    Got email sort of working

--- hosts/therock/cyan-network-config.nix
+++ hosts/therock/cyan-network-config.nix
@@ -6,9 +6,139 @@ let
 in
 {
 
+
+  # ==================================================
+  # EMAIL
+  # ==================================================
+  users.users."vmail" = {
+    createHome = true;
+    home = "/var/spool/mail/vmail";
+    uid = 994; # ...??? necessary?
+  };
+  users.groups."vmail" = {
+    gid = 993; 
+  };
+  users.users."postfix" = {
+    createHome = true;
+    home = "/var/spool/postfix";
+  };
+  services.postfix = {
+    enable = true;
+    enableSubmission = true;
+    enableSubmissions = true;
+
+    settings.main = {
+      # myhostname = "ip-192.168.200.1";
+      # mydomain = "localhost";
+      
+      # NOTE: these work with cyan.arpa domains in all of the usernames
+      # everywhere
+      # myhostname = "cyan.arpa";
+      # mydomain = "cyan";
+      virtual_mailbox_domains = "cyan.arpa";
+
+      # myhostname = "cyan.arpa";
+      # mydomain = "cyan";
+      # virtual_mailbox_domains = "192.168.200.1";
+      
+      virtual_uid_maps = "static:994";
+      virtual_gid_maps = "static:993";
+      # virtual_mailbox_domains = "192.168.200.1";
+      # virtual_mailbox_domains = "[192.168.200.1]";
+      virtual_mailbox_base = "/var/spool/mail/vmail";
+      virtual_mailbox_maps = "hash:/etc/email/virtual_mailbox_maps";
+      mailbox_transport = "lmtp:unix:/var/spool/postfix/dovecot-lmtp";
+      virtual_transport = "lmtp:unix:/var/spool/postfix/dovecot-lmtp";
+
+      # local_transport = "lmtp:unix:/var/spool/postfix/dovecot-lmtp";
+      # local_recipient_maps = "hash:/etc/email/virtual_mailbox_maps";
+
+      
+      smtpd_sasl_type = "dovecot";
+      smtpd_sasl_path = "/var/spool/postfix/auth";
+      smtpd_sasl_auth_enable = "yes";
+      smtpd_recipient_restrictions = "permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination";
+
+      resolve_numeric_domain = "yes"; # allow ip addresses
+    };
+
+    # hostname = "ip-192.168.200.1";
+    # domain = "192.168.200.1";
+
+  };
+  # https://wiki.nixos.org/wiki/Dovecot
+  services.dovecot2 = {
+    enable = true;
+    enablePAM = false;
+    # NOTE: password file most be mode 600 and owner/group dovecot2
+    createMailUser = true;
+    mailUser = "vmail";
+    mailGroup = "vmail";
+    mailLocation = "maildir:~/Maildir";
+    mailboxes = {
+      # use rfc standard https://apple.stackexchange.com/a/201346
+      All = { auto = "create"; autoexpunge = null; specialUse = "All"; };
+      Archive = { auto = "create"; autoexpunge = null; specialUse = "Archive"; };
+      Drafts = { auto = "create"; autoexpunge = null; specialUse = "Drafts"; };
+      Flagged = { auto = "create"; autoexpunge = null; specialUse = "Flagged"; };
+      Junk = { auto = "create"; autoexpunge = "60d"; specialUse = "Junk"; };
+      Sent = { auto = "create"; autoexpunge = null; specialUse = "Sent"; };
+      Trash = { auto = "create"; autoexpunge = "60d"; specialUse = "Trash"; };
+    };
+      # auth_username_format = %Lu
+    
+        # (userdb) args = uid=vmail gid=vmail username_format=%u home=/var/spool/mail/vmail/%d/%n
+    extraConfig = ''
+      auth_default_realm = cyan.arpa
+      auth_username_format = %n
+
+      auth_mechanisms = plain
+      passdb {
+        driver = passwd-file
+        args = /etc/email/dovecotusers
+      }
+
+      userdb {
+        driver = static
+        # the full e-mail address inside passwd-file is the username (%u)
+        # user@example.com
+        # %d for domain_name %n for user_name
+        args = uid=vmail gid=vmail username_format=%n home=/var/spool/mail/vmail/%d/%n
+      }
+
+      # connection to postfix via lmtp
+      service lmtp {
+        unix_listener /var/spool/postfix/dovecot-lmtp {
+          mode = 0600
+          user = postfix
+          group = postfix
+        }
+      }
+      service auth {
+        unix_listener /var/spool/postfix/auth {
+          mode = 0600
+          user = postfix
+          group = postfix
+        }
+      }
+    '';
+    enableLmtp = true;
+  };
+  # ==================================================
+  # /EMAIL
+  # ==================================================
+
+
+  
+
   networking.firewall.allowedTCPPorts = [ 
-    80 
-    8000
+    80 # http
+    
+    25 # smtp
+    465 # smtps
+
+    # 993 # divecot imaps
+    143 # dovecot imap
   ];
 
   networking.firewall.allowedUDPPorts = [ 51830 ];
@@ -66,6 +196,9 @@ in
   environment.systemPackages = with pkgs; [
     unixtools.net-tools
     wireguard-tools
+    qrencode
+
+    mailutils
   ];
   
   services.nginx = {
